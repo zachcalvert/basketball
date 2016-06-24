453cc822fe3b0571ba825eeaeabec22793fc0e8a
Zach Calvert <zachc@darkhorse.com>
2016-06-23 17:00:57 -0700

    anticipated requirements

---
+++ basketball/basketball/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name='logout'),
+)
